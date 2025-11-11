1b71477b8db0d7054d2b42635c2de116333f2913
m-a-r-t-in <krucek.martin@gmail.com>
2025-11-11 10:11:37 +0100

    new build

--- docs/assets/js/lunr/lunr-store.js
+++ docs/assets/js/lunr/lunr-store.js
@@ -6,13 +6,13 @@ var store = [{
         "teaser": null
       },{
         "title": "ACUVEG Project - Acoustic Noise Attenuation by Vegetation",
-        "excerpt":"The **ACUVEG** project studies how vegetation structure affects sound attenuation in various forest types. The aim of the project \"***Determination and optimization of vegetation belts of natural woody composition serving to traffic noise attenuation.***\" is to develop a public methodology on the use of vegetation belts for traffic noise attenuation...","categories": [],
+        "excerpt":"The ACUVEG project studies how vegetation structure affects sound attenuation in various forest types. The aim of the project “Determination and optimization of vegetation belts of natural woody composition serving to traffic noise attenuation.” is to develop a public methodology on the use of vegetation belts for traffic noise attenuation...","categories": [],
         "tags": [],
         "url": "/acuveg_database/",
         "teaser": null
       },{
         "title": "Project Details",
-        "excerpt":"## Introduction: The main concept of the prject is to identify suitable spatial and species compositions of vegetative strips of trees, which will be deliberately established to mitigate noise from transportation. The project's goal is to conduct repeated measurements at the same locations under different conditions. All research will be...","categories": [],
+        "excerpt":"Introduction: The main concept of the prject is to identify suitable spatial and species compositions of vegetative strips of trees, which will be deliberately established to mitigate noise from transportation. The project’s goal is to conduct repeated measurements at the same locations under different conditions. All research will be carried...","categories": [],
         "tags": [],
         "url": "/acuveg_database/project_details/",
         "teaser": null
