fd12cb036b1dfc77ba7e0168f93eed4e4af5b3f9
Krůček Martin <krucek@vukoz.cz>
2026-03-03 10:37:39 +0100

    lokal build to deploy

---
+++ docs/assets/js/lunr/lunr-store.js
@@ -0,0 +1,19 @@
+var store = [{
+        "title": "Database",
+        "excerpt":"Plnou verzi databáze lze stáhnout zde. Obsahuje detailní popis lesního porostu, měření útlumu hluku a LiDAR data (ve formě předzpracovaných bodových mračen) a odvozené proměnné.   Popis dat  Bude doplněno podle finální verze databáze …  ","categories": [],
+        "tags": [],
+        "url": "/acuveg_database/database/",
+        "teaser": null
+      },{
+        "title": "ACUVEG - Útlum hluku vegetací",
+        "excerpt":"Projekt ACUVEG zkoumá, jak struktura vegetace ovlivňuje útlum zvuku v různých typech lesů. Cílem projektu “Stanovení a optimalizace vegetačních pásů přirozené dřevinné skladby sloužících k útlumu hluku z dopravy” je vyvinout veřejně dostupnou metodiku využití vegetačních pásů pro tlumení dopravního hluku a odpovídající ilustrační databázi. Další dílčí výzkumné poznatky budou...","categories": [],
+        "tags": [],
+        "url": "/acuveg_database/",
+        "teaser": null
+      },{
+        "title": "O projektu",
+        "excerpt":"Úvod: Hlavní koncepcí projektu je identifikovat vhodné prostorové a druhové složení vegetačních pásů dřevin, které budou cíleně zakládány za účelem zmírnění dopravního hluku. Cílem projektu je provádět opakovaná měření na stejných lokalitách za různých podmínek. Veškerý výzkum bude realizován s ohledem na budoucí bezpečnost silniční a železniční dopravy tak, aby...","categories": [],
+        "tags": [],
+        "url": "/acuveg_database/project_details/",
+        "teaser": null
+      },]
